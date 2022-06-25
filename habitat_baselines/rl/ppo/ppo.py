6a71e418dad714487a114e5d0b3bb67728713f13
Naoki Yokoyama <naokiyokoyama@github.com>
2022-06-25 09:53:07 -0500

    sum entropy if it hasnt been done yet

--- habitat_baselines/rl/ppo/ppo.py
+++ habitat_baselines/rl/ppo/ppo.py
@@ -116,6 +116,8 @@ class PPO(nn.Module):
                     batch["masks"],
                     batch["actions"],
                 )
+                if dist_entropy.shape[1] != 1:
+                    dist_entropy = dist_entropy.sum(1, keepdims=True)
 
                 ratio = torch.exp(action_log_probs - batch["action_log_probs"])
                 surr1 = ratio * batch["advantages"]
